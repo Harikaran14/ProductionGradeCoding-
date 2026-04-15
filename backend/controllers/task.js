9738d848486472d4f759c22f0f7a5bf862c2a60e
Harikaran <harikaran2310104@ssn.edu.in>
2026-04-15 14:45:46 +0530

    Update successfully Implemented

--- backend/controllers/task.js
+++ backend/controllers/task.js
@@ -16,7 +16,7 @@ const createTask = async (req,res)=>{
     });
 }
 catch(error){
-
+    console.log(error);
         res.status(500).json({
             message: "Server error"
         });
@@ -39,4 +39,39 @@ const getTask = async (req,res)=>{
         });
     }
 }
-module.exports={createTask, getTask};
+const updateTask = async (req,res)=>{
+try{
+    const taskid=req.params.id;
+    const task =await Task.findById(taskid);
+    if (!task){
+        return res.status(404).json({
+            message:"No such task exists"
+        });
+    
+    } 
+    if (task.user.toString()!= req.user._id.toString()){
+        return res.status(403).json({
+            message:"Not Authorized to update the task"
+        });
+    
+    }
+
+    task.title= req.body.title || task.title;
+    task.description=req.body.description || task.description;
+    task.status = req.body.status || task.status;
+    await task.save();
+
+    res.status(200).json({
+        message:"Updated successfully ", task
+    });
+
+}
+catch(error){
+    console.log(error);
+        res.status(500).json({
+            message: "Server error"
+        });
+}
+
+}
+module.exports={createTask, getTask, updateTask};
--- backend/routes/taskroute.js
+++ backend/routes/taskroute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router =express.Router();
 const protect= require("../middleware/authm");
-const { createTask , getTask} = require("../controllers/task");
+const { createTask , getTask, updateTask} = require("../controllers/task");
 console.log("Task route loaded");
 router.post("/",protect, createTask);
 router.get("/test",(req,res)=>{
@@ -10,4 +10,5 @@ router.get("/test",(req,res)=>{
     });
 })
 router.get("/",protect, getTask);
+router.put("/:id",protect,updateTask);
 module.exports = router;
